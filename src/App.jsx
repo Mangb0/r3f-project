a845698bc718701925e10ded6a94e3e1f5e3a6a3
Mangb0 <whdudghks45629@gmail.com>
2023-12-20 23:37:02 +0900

    test: main page 구성 가이드라인

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,37 @@ import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </Canvas>
+    // <Canvas camera={{ position: [3, 3, 3] }}>
+    //   <mesh>
+    //     <boxGeometry />
+    //     <meshNormalMaterial />
+    //   </mesh>
+    // </Canvas>
+    <>
+      <div>react router</div>
+      <div className="container">
+        <div>
+          <div>card1</div>
+          <h2>project title</h2>
+          <h4>content</h4>
+        </div>
+        <div>
+          <div>card2</div>
+          <h2>project title</h2>
+          <h4>content</h4>
+        </div>
+        <div>
+          <div>card3</div>
+          <h2>project title</h2>
+          <h4>content</h4>
+        </div>
+        <div>
+          <div>card4</div>
+          <h2>project title</h2>
+          <h4>content</h4>
+        </div>
+      </div>
+    </>
   );
 }
 
