08f861a7c0199425092a09984364717fada610ac
Mangb0 <whdudghks45629@gmail.com>
2023-12-23 16:08:33 +0900

    Add pokemon card

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
 import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    // <Canvas camera={{ position: [3, 3, 3] }}>
-    //   <mesh>
-    //     <boxGeometry />
-    //     <meshNormalMaterial />
-    //   </mesh>
-    // </Canvas>
-    <>
-      <div>react router</div>
-      <div className="container">
-        <div>
-          <div>card1</div>
-          <h2>project title</h2>
-          <h4>content</h4>
-        </div>
-        <div>
-          <div>card2</div>
-          <h2>project title</h2>
-          <h4>content</h4>
-        </div>
-        <div>
-          <div>card3</div>
-          <h2>project title</h2>
-          <h4>content</h4>
-        </div>
-        <div>
-          <div>card4</div>
-          <h2>project title</h2>
-          <h4>content</h4>
-        </div>
-      </div>
-    </>
+    // <>
+    //   <div>react router</div>
+    //   <div className="container">
+    //     <div>
+    //       <div>card1</div>
+    //       <h2>project title</h2>
+    //       <h4>content</h4>
+    //     </div>
+    //     <div>
+    //       <div>card2</div>
+    //       <h2>project title</h2>
+    //       <h4>content</h4>
+    //     </div>
+    //     <div>
+    //       <div>card3</div>
+    //       <h2>project title</h2>
+    //       <h4>content</h4>
+    //     </div>
+    //     <div>
+    //       <div>card4</div>
+    //       <h2>project title</h2>
+    //       <h4>content</h4>
+    //     </div>
+    //   </div>
+    // </>
+    <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
   );
 }
 
---
+++ src/components/Fish.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+export function Fish({ hovered, ...props }) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/models/Fish.gltf");
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    const anim = hovered ? "Wave" : "Idle";
+    actions[anim].reset().fadeIn(0.5).play();
+    return () => actions[anim].fadeOut(0.5);
+  }, [hovered]);
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="CharacterArmature">
+          <primitive object={nodes.Root} />
+          <skinnedMesh
+            name="Fish"
+            geometry={nodes.Fish.geometry}
+            material={materials.Atlas}
+            skeleton={nodes.Fish.skeleton}
+          />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/models/Fish.gltf");
