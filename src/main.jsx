614c1ddef925a18d41f91c0183679c775f8472ff
Mangb0 <whdudghks45629@gmail.com>
2024-01-09 22:42:50 +0900

    local 설정 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/r3f-project">
+  // <BrowserRouter basename="/r3f-project"> when deploying to github pages
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
